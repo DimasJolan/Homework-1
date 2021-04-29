2554813a75af77e3a89b0b40240ad32547a4e920
DimasJolan <71343073+DimasJolan@users.noreply.github.com>
2021-04-29 15:03:21 +0300

    Add files via upload

---
+++ myjavascript.js
@@ -0,0 +1,9 @@
+"use strict";
+
+document.write("**************");
+document.write("Обьем цилиндра с площадью основы S = 15, радиусом R = 17 и высотой H = 23 равен:");
+document.write("--------------------");
+console.log(Math.PI * Math.pow(17,2) * 23)
+document.write("V = 20882.166368411356");
+document.write("--------------------");
+document.write("end.");
